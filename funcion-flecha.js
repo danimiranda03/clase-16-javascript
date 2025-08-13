868f1193c26784d54cf157b0e3610112480718c0
danimiranda03 <dmm260996@gmail.com>
2025-08-12 19:42:07 -0500

    creacin archivo javascript

---
+++ funcion-flecha.js
@@ -0,0 +1,14 @@
+function nombreDeLaFuncion(n1,n2){
+    console.log ("Los números son :", n1 ,n2)
+}
+
+
+const variable =()=>{
+
+}
+
+const imprimirNombre =(nombre)=>{
+    return "Hola "+ nombre;
+}
+resultado=imprimirNombre("Daniela");
+console.log(resultado);
